2e8dae4b5897f64d438522ecc7e5557b66f5cda1
USER <USER@DESKTOP-54G52SJ>
2022-01-08 13:34:25 +0530

    First Commit

---
+++ My Project/src/Project/JayMaaSaraswati.java
@@ -0,0 +1,11 @@
+package Project;
+
+public class JayMaaSaraswati {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Jay Maa Saraswati");
+		System.out.println("Tabrez");
+	}
+
+}
